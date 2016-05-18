6bfd381194a9867b18913465ce8c9ba8b1ea7532
MUSTAFA AKILLI <makilli@gtu.edu.tr>
2016-05-18 20:59:59 +0300

    First Commit
    I started HW09

---
+++ src/main/java/tr/edu/gtu/mustafa/akilli/cse222/hw09/App.java
@@ -0,0 +1,13 @@
+package tr.edu.gtu.mustafa.akilli.cse222.hw09;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
